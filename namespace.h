2415ff0c956d2c2b1221d33db24c9c34d0dff552
Mandresy33 <167546988+Mandresy33@users.noreply.github.com>
2025-02-06 13:16:40 +0300

    Add files via upload

---
+++ namespace.h
@@ -0,0 +1,11 @@
+#ifndef NAMESPACE_H_INCLUDED
+#define NAMESPACE_H_INCLUDED
+namespace isa
+{
+    #include "Rationnel.h"
+    #include "Complex.h"
+    #include "RationnalComplex.h"
+    #include "Arithmetique.h"
+};
+
+#endif // NAMESPACE_H_INCLUDED
